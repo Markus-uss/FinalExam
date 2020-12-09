cec2afece4e388f43209ef93ac53017b41c1cff9
Markus Lu <markuslu.bc@gmail.com>
2020-12-09 14:03:11 -0800

    Displayed Images on Id 'thumb-bar'

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ let hiddenElements = document.querySelectorAll('label, input, button')
 console.log(hiddenElements)
 console.log('testing')
 
+
 for (elements of hiddenElements) {
     elements.classList.add("hidden");
 }
 
-for (numbers in range(5)) {
-    console.log(numbers)
+
+for (i = 1; i <= 5; i++) {
+    let img = document.createElement('img');
+    img.src = ('images/pic' + i + '.jpg');
+    document.getElementById("thumb-bar").appendChild(img);
 }
