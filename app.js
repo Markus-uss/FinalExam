cf48da4ab500fca26baa1a4612c1100ed6b280dc
Markus Lu <markuslu.bc@gmail.com>
2020-12-09 13:26:24 -0800

    Set (label, input, buttons) class hidden by default.

---
+++ app.js
@@ -0,0 +1,11 @@
+let hiddenElements = document.querySelectorAll('label, input, button')
+console.log(hiddenElements)
+console.log('testing')
+
+for (elements of hiddenElements) {
+    elements.classList.add("hidden");
+}
+
+for (numbers in range(5)) {
+    console.log(numbers)
+}
